f191b2dea47075ea91683e3c2b373894a8ac3650
Mikhail9030 <gulaevmihail112@gmail.com>
2020-12-08 22:16:00 +0300

    Commit 1

---
+++ IdeaProjects/untitled/src/project1.java
@@ -0,0 +1,8 @@
+class Scratch {
+    public static void main(String[] args) {
+        long current = 2000000000;
+        long transfer = 500000000;
+        long total = current + transfer;
+        System.out.println(total);
+    }
+}
